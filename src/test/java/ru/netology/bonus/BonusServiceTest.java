a54caa68b976c4caf68b4e21a9eb1c68b142cf79
Maksim Laptenko <laptenko0811@icloud.com>
2021-04-21 16:41:55 +0300

    add JoCoCo.check

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -8,10 +8,7 @@ class BonusServiceTest {
 
     @ParameterizedTest(name = "{index} - " + "{0}")
     @CsvSource(value = {
-            "'registered user, bonus under limit', 100060, true, 30",
-            "'registered user, bonus over limit', 100000060, true, 500",
-            "'no registered user, bonus over limit', 100060, false, 10",
-            "'no registered user, bonus over limit', 100000060, false, 500" }
+            "'registered user, bonus under limit', 100060, true, 30"}
     )
     void calculate(String testName, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
